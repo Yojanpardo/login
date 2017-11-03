c7ddeda6f8c9c6a47f4c8c9ecd0574d87c6961c7
Yojan Pardo <yojan.pardo@gmail.com>
2017-11-03 18:43:00 -0500

    termino mi sistema e login y logiout, todo funciona correctamente y dejo ésto aquí para poder usarlo más delante

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^login/$', views.authentication, name='login'),
+	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^logout$', auth_views.logout, {'next_page': '/'} , name='logout'),
 	url(r'^$',views.home, name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,18 +6,21 @@ from django.contrib.auth import authenticate, login
 
 def authentication(request):
 	if request.method == 'POST':
-		action = request.POST.get('action')
 		username = request.POST.get('username', None)
 		password = request.POST.get('password', None)
-		if action == 'login':
-			user = authenticate(username=username, password=password)
-			login(request,user)
-			return redirect('/')
-		elif action == 'signup':
-			user = User.objects.create_user(username=username, password=password)
-			user.save()
-
+		user = authenticate(username=username, password=password)
+		login(request,user)
+		return redirect('/')
 	return render(request,'login.html', {})
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def signup(request):
+	if request.method == 'POST':
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+		user = User.objects.create_user(username=username, password=password)
+		user.save()
+		return redirect('/login')
+	return render(request,'signup.html', {})
